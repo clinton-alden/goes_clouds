6584618f475b8111761778245031e0a6a9fb2757
Clinton Alden <cdalden@uw.edu>
2025-03-27 16:52:54 -0700

    added files

--- data_download/download.sh
+++ data_download/download.sh
@@ -1,1 +1,1 @@
-python ./download-goes.py --bucket noaa-goes16 --year 2023 --month 6 --days 1 10 --product ABI-L1b-RadC --channel C13 --bounds 37 41 -109 -104 --dir /storage/cdalden/goes
+python ./download-goes.py --bucket noaa-goes16 --year 2023 --month 6 --days 1 2 --product ABI-L1b-RadC --channel C13 --bounds -109 37 -104 41 --dir /storage/cdalden/goes
