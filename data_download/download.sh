8902d692bf6135fb650173d25399a18bd8ca9458
Clinton Alden <cdalden@uw.edu>
2025-03-31 14:15:34 -0700

    download channel 05

--- data_download/download.sh
+++ data_download/download.sh
@@ -1,6 +1,6 @@
 # Suppress the RuntimeWarning for the cfgrib engine
 # export PYTHONWARNINGS="ignore:Engine 'cfgrib' loading failed"
 
-python ./download-goes.py --bucket noaa-goes16 --year 2023 --month 6 --days 1 20 --product ABI-L1b-RadC --channel C02 --bounds -109 37 -104 41 --dir /storage/cdalden/goes
+python ./download-goes.py --bucket noaa-goes16 --year 2023 --month 6 --days 1 20 --product ABI-L1b-RadC --channel C05 --bounds -109 37 -104 41 --dir /storage/cdalden/goes
 
 # python ./goes_nc_to_zarr.py
