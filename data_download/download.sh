74fea462dc02406cea9eaec4d1b5958687243b57
Clinton Alden <cdalden@uw.edu>
2025-06-27 16:17:45 -0700

    download updates

--- data_download/download.sh
+++ data_download/download.sh
@@ -3,6 +3,8 @@
 
 # IMPORTANT - use goes_old environment
 # I don't fully remember but I think this env manually imported some goes-ortho code
-python ./download-goes.py --bucket noaa-goes16 --year 2022 --month 7 --days 13 13 --product ABI-L1b-RadC --channel C13 --bounds -124 48 -121 49 --dir /storage/cdalden/goes/washington
+for channel in C2 C5 C13; do
+    python ./download-goes.py --bucket noaa-goes16 --year 2022 --month 7 --days 13 31 --product ABI-L1b-RadC --channel $channel --bounds -124 48 -121 49 --dir /storage/cdalden/goes/washington
+done
 
 # python ./goes_nc_to_zarr.py
--- data_download/download_workflow.md
+++ data_download/download_workflow.md
@@ -2,3 +2,11 @@
 
 1. download GOES data using `/data_download/download.sh`
 2. combine GOES data from same product into one zarr file using `/data_download/goes_nc_to_zarr.ipynb`
+
+
+
+
+
+Current download status:
+Need Channels 2/5/13 for W WA for 13 July - 30 Sep 2022
+downloading... all channels for july 2022
