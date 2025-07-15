3bc228368b70750bbc27e1ad8fcca6476075d0b4
Clinton Alden <cdalden@uw.edu>
2025-07-15 13:37:44 -0700

    new downloads

--- data_download/download.sh
+++ data_download/download.sh
@@ -6,9 +6,10 @@
 
 # IMPORTANT - channels need to be 3 digits (ie C02, C05, or C13)
 
-
-for channel in C02 C05 C13; do
-    python ./download-goes.py --bucket noaa-goes17 --year 2022 --month 7 --days 13 31 --product ABI-L1b-RadC --channel $channel --bounds -124 47 -121 49 --dir /storage/cdalden/goes/washington
+for day in {13..31}; do
+    for channel in C02 C05 C13; do
+        python ./download-goes.py --bucket noaa-goes17 --year 2022 --month 7 --days $day $day --product ABI-L1b-RadC --channel $channel --bounds -124 47 -121 49 --dir /storage/cdalden/goes/washington
+    done
 done
 
 # python ./goes_nc_to_zarr.py
