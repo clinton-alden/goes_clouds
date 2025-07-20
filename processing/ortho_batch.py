3f0fcf4612fea2c4f889f12ea6a3279129846118
Clinton Alden <cdalden@uw.edu>
2025-07-20 15:24:57 -0700

    new downloads

--- processing/ortho_batch.py
+++ processing/ortho_batch.py
@@ -17,22 +17,21 @@ def process_files(root_dir):
                 bounds = (-124, 48, -121, 49)
                 api_key = "41d14aae7e761c0de3e8f99aa4fd24d9"
 
-            if 'ortho' in netcdf_path:
-                print(f"File {goes_image_path} already ortho'd, skipping.")
-            else:
-                orthorectify_modded.ortho(
-                    goes_image_path,
-                    data_vars,
-                    bounds,
-                    api_key,
-                    new_goes_filename,
-                    dem_filepath=None,
-                    demtype="SRTMGL3",
-                    keep_dem=False,
-                )
-
-                # Optionally delete the original NetCDF file
-                os.remove(netcdf_path)
+                if 'ortho' in netcdf_path:
+                    print(f"File {goes_image_path} already ortho'd, skipping.")
+                else:
+                    orthorectify_modded.ortho(
+                        goes_image_path,
+                        data_vars,
+                        bounds,
+                        api_key,
+                        new_goes_filename,
+                        dem_filepath='temp_SRTMGL3_DEM.tif',
+                        demtype="SRTMGL3",
+                        keep_dem=True,
+                    )
+                    # Optionally delete the original NetCDF file
+                    os.remove(netcdf_path)
 
 if __name__ == "__main__":
     # Check if the root directory is provided as a command-line argument
