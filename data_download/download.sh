0f491d1b1c298478ca53556fe302362af8b2da1a
Clinton Alden <cdalden@uw.edu>
2025-07-21 00:05:36 -0700

    new downloads/processing

--- data_download/download.sh
+++ data_download/download.sh
@@ -12,10 +12,10 @@
 #     done
 # done
 
-for day in {1..1}; do
+for day in {6..6}; do
     for channel in C02 C05 C13; do
         python ./download-goes.py --bucket noaa-goes17 --year 2022 --month 8 --days $day $day --product ABI-L1b-RadC --channel $channel --bounds -124 47 -121 49 --dir /storage/cdalden/goes/washington
-        python ../processing/ortho_batch.py /storage/cdalden/goes/washington/2022/08/$day/ABI-L1b-RadC/$channel/
+        # python ../processing/ortho_batch.py /storage/cdalden/goes/washington/goes17/2022/08/$day/ABI-L1b-RadC/$channel/
     done
 done
 
