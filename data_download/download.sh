acb300843fcbc37d96ebebcddb8ac69e6f154cb1
clinton-alden <cdalden@uw.edu>
2025-09-09 11:20:06 -0700

    clean up download script

--- data_download/download.sh
+++ data_download/download.sh
@@ -1,19 +1,6 @@
-# Suppress the RuntimeWarning for the cfgrib engine
-# export PYTHONWARNINGS="ignore:Engine 'cfgrib' loading failed"
-
-
-# IMPORTANT - channels need to be 3 digits (ie C02, C05, or C13)
-
-
-# for day in {1..31}; do
-#     for channel in C02 C05 C13; do
-#         python ./download-goes.py --bucket noaa-goes17 --year 2022 --month 8 --days $day $day --product ABI-L1b-RadC --channel $channel --bounds -125 46 -121 49 --dir /storage/cdalden/goes/washington
-#         # python ../processing/ortho_batch.py /storage/cdalden/goes/washington/goes17/2022/08/$day/ABI-L1b-RadC/$channel/
-#     done
-# done
-
 #!/bin/bash
 
+# IMPORTANT - channels need to be 3 digits (ie C02, C05, or C13)
 # Loop through days and channels
 for day in $(seq 1 28); do
     for channel in C02 C05 C13; do
